1395f76c6099254f47807461326b3a641d56a318
Saskia Opdam <opdamsaskia@icloud.com>
2023-07-02 19:08:19 +0200

    backup - operators

---
+++ operators/main.py
@@ -0,0 +1,38 @@
+# Do not modify these lines
+__winc_id__ = 'd0d3cdcefbb54bc980f443c04ab3a9eb'
+__human_name__ = 'operators'
+
+# Add your code after this line
+
+#1
+switzerland_most_spoken_language = "Swiss German"
+spain_most_spoken_language = "Castillian Spanish"
+print(switzerland_most_spoken_language == spain_most_spoken_language)
+
+#2
+switzerland_most_prevalent_religion = "Roman Catholic"
+spain_most_prevalent_religion = "Roman Catholic"
+print(switzerland_most_prevalent_religion == spain_most_prevalent_religion)
+
+#3
+switzerland_capital_length = len("Bern")
+spain_capital_length = len("Madrid")
+print(switzerland_capital_length != spain_capital_length)
+
+#4
+switzerland_GDP = 590710000000
+spain_GDP = 1714860000000
+print(switzerland_GDP > spain_GDP)
+
+#5
+switzerland_population_growth = 0.0065
+spain_population_growth = -0.0003
+print(switzerland_population_growth and spain_population_growth < 0.01)
+
+#6
+switzerland_population_count = 8500000
+spain_population_count = 47300000
+print(switzerland_population_count > 10000000 or spain_population_count > 10000000)
+
+#7
+print(switzerland_population_count and spain_population_count > 10000000)
