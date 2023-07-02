52ce06db22112163627a2b3fc6e3bdfa962ed89c
Saskia Opdam <opdamsaskia@icloud.com>
2023-07-02 19:11:32 +0200

    backup - variables

---
+++ variables/main.py
@@ -0,0 +1,20 @@
+# Do not modify these lines
+__winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
+__human_name__ = 'variables'
+
+# Add your code after this line
+# name = "Bob"
+# age = 54
+# isIll = True
+
+# print(type(name))
+# print(type(age))
+# print(type(isIll))
+
+welkom = 'welkom bij deze cursus'
+leeftijd = 31
+controle = 10 > 9
+
+print(controle)
+print(welkom)
+print(leeftijd)
