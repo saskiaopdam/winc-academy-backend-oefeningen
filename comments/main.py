4e0fc98c50213d55864200a513635b4f8cb41dd0
Saskia Opdam <opdamsaskia@icloud.com>
2023-07-02 19:03:30 +0200

    backup - comments

---
+++ comments/main.py
@@ -0,0 +1,26 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+productPrice = 10 # base gross price of one item
+tax10 = 1.10 # adding 10% tax
+
+# total price of one item
+# including 10% tax
+totalPrice = productPrice * tax10
+
+"""
+example
+of a
+multiline comment
+"""
+
+"""
+another example
+of a
+multiline comment
+"""
+
+
